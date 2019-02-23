6c8b6cf403a13ffe00e28fd1d8fac564bdcda5b6
yevkk <yevkk@users.noreply.github.com>
2019-02-23 16:06:33 +0200

    Type defined

---
+++ lab2a/cmake-build-debug/CMakeFiles/lab2a.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab2a.dir/main.cpp.obj"
+  "lab2a.pdb"
+  "lab2a.exe"
+  "lab2a.exe.manifest"
+  "liblab2a.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab2a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
