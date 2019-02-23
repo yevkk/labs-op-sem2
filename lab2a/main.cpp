5669a949bbeda91b64cd9a263fc6ff99acb3e7e4
yevkk <yevkk@users.noreply.github.com>
2019-02-23 18:39:00 +0200

    create_empty for list, vector and array added

--- lab2a/main.cpp
+++ lab2a/main.cpp
@@ -1,13 +1,50 @@
 #include <iostream>
+#include <vector>
 
-struct Circle{
+using namespace std;
+
+const int N = 10;
+struct Circle {
     struct {
-        double x,y;
+        double x, y;
     } center;
     double radius;
 };
 
+struct Node {
+    Circle circle;
+    Node *prev = nullptr;
+    Node *next = nullptr;
+};
+
+struct List {
+    Node *head = nullptr, *tail = nullptr;
+} queue_list;
+
+void create_empty_list(List queue) {
+    while (queue.head != queue.tail){
+        queue.tail = queue.tail->next;
+        delete queue.tail;
+    }
+}
+
+void create_empty_array(int queue_size){
+    queue_size = 0;
+}
+
+void create_empty_array(vector<Circle> queue) {
+    queue.clear();
+}
 
 int main() {
+    struct{
+      Circle data[N];
+      int size = 0;
+    }queue_array;
+
+
+    vector<Circle> queue_vector;
+
+
     return 0;
 }
