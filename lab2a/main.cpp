7e2359d07538db7c87f775cd655d2ea40733ea90
yevkk <yevkk@users.noreply.github.com>
2019-02-23 20:17:27 +0200

    void enqueue() for list, vector and array added

--- lab2a/main.cpp
+++ lab2a/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-const int N = 10;
+const int MAX_SIZE = 10;
 struct Circle {
     struct {
         double x, y;
@@ -48,9 +48,35 @@ bool is_empty_list(vector<Circle> queue) {
     return (queue.size() == 0) ? true : false;
 }
 
+void enqueue_list(List queue, double x, double y, double radius) {
+    Node *ptr = new Node;
+    ptr->next = nullptr;
+    ptr->prev = queue.tail;
+    ptr->circle.center.x = x;
+    ptr->circle.center.y = y;
+    ptr->circle.radius = radius;
+    queue.tail = ptr;
+}
+
+void enqueue_array(Circle *queue_data, int &queue_size, double x, double y, double radius) {
+    if (queue_size + 1 > MAX_SIZE) {
+        cout << "element cannot be added, queue is full";
+        return;
+    }
+    queue_data[queue_size].center.x = x;
+    queue_data[queue_size].center.y = y;
+    queue_data[queue_size].radius = radius;
+    queue_size++;
+}
+
+void enqueue_vector(vector<Circle> queue, double x, double y, double radius) {
+    Circle circle = {x, y, radius};
+    queue.insert(queue.begin(), circle);
+}
+
 int main() {
     struct {
-        Circle data[N];
+        Circle data[MAX_SIZE];
         int size = 0;
     } queue_array;
 
