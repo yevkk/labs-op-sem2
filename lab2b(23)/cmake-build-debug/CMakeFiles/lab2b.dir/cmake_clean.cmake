0fc11244baf5d3b037a8e72a8d3c1d3e92b1f6f7
yevkk <yevkk@users.noreply.github.com>
2019-02-25 16:38:40 +0200

    folders renamed

---
+++ lab2b(23)/cmake-build-debug/CMakeFiles/lab2b.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab2b.dir/main.cpp.obj"
+  "lab2b.pdb"
+  "lab2b.exe"
+  "lab2b.exe.manifest"
+  "liblab2b.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab2b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
