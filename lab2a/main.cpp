0b85a36e167749c29f50b520e3089e3f243ae086
yevkk <yevkk@users.noreply.github.com>
2019-02-25 15:15:52 +0200

    benchmark added;

--- lab2a/main.cpp
+++ lab2a/main.cpp
@@ -2,10 +2,13 @@
 #include <vector>
 #include <string>
 #include <random>
+#include <chrono>
+#include <functional>
 
 using namespace std;
+using namespace std::chrono;
 
-const int MAX_SIZE = 10;
+const int MAX_SIZE = 100;
 struct Circle {
     struct {
         double x, y;
@@ -22,7 +25,7 @@ struct List {
     Node *head = nullptr, *tail = nullptr;
 };
 
-double rand_num(double max){
+double rand_num(double max) {
     static random_device rd;
     static mt19937 mt(rd());
     static uniform_real_distribution<double> dist(0, max);
@@ -57,7 +60,7 @@ bool is_queue_empty(vector<Circle> queue) {
     return (queue.size() == 0);
 }
 
-void enqueue(List &queue, double x, double y, double radius) {
+void enqueue(List &queue, double x, double y, double radius, int flag = 1) {
     Node *ptr = new Node;
     ptr->circle.center.x = x;
     ptr->circle.center.y = y;
@@ -70,60 +73,67 @@ void enqueue(List &queue, double x, double y, double radius) {
         queue.tail->next = ptr;
         queue.tail = ptr;
     }
-    cout << "Element ((" << x << ", " << y << "), " << radius << ") added;\n";
+    if (flag) cout << "Element ((" << x << ", " << y << "), " << radius << ") added;\n";
 }
 
-void enqueue(Circle *queue_data, int &queue_size, double x, double y, double radius) {
+void enqueue(Circle *queue_data, int &queue_size, double x, double y, double radius, int flag = 1) {
     if (queue_size + 1 > MAX_SIZE) {
-        cout << "Element cannot be added, queue is full\n";
+         if (flag) cout << "Element cannot be added, queue is full\n";
         return;
     }
     queue_data[queue_size].center.x = x;
     queue_data[queue_size].center.y = y;
     queue_data[queue_size].radius = radius;
     queue_size++;
-    cout << "Element ((" << x << ", " << y << "), " << radius << ") added;\n";
+    if (flag) cout << "Element ((" << x << ", " << y << "), " << radius << ") added;\n";
 }
 
-void enqueue(vector<Circle> &queue, double x, double y, double radius) {
+void enqueue(vector<Circle> &queue, double x, double y, double radius, int flag = 1) {
     Circle circle = {x, y, radius};
     queue.push_back(circle);
-    cout << "Element ((" << x << ", " << y << "), " << radius << ") added;\n";
+    if (flag) cout << "Element ((" << x << ", " << y << "), " << radius << ") added;\n";
 }
 
-void dequeue(List &queue) {
+void dequeue(List &queue, int flag = 1) {
     if (is_queue_empty(queue)) {
-        cout << "Element cannot be removed, queue is empty\n";
+        if (flag) cout << "Element cannot be removed, queue is empty\n";
         return;
     }
     Node *ptr = queue.head->next;
-    cout << "Element ((" << queue.head->circle.center.x << ", " << queue.head->circle.center.y << "), " << queue.head->circle.radius << ")";
+    if (flag)
+        cout << "Element ((" << queue.head->circle.center.x << ", " << queue.head->circle.center.y << "), "
+             << queue.head->circle.radius << ")";
     delete queue.head;
     queue.head = ptr;
-    cout  << "removed;\n";
+    if (flag) cout << "removed;\n";
 }
 
-void dequeue(Circle *queue_data, int &queue_size) {
+void dequeue(Circle *queue_data, int &queue_size, int flag = 1) {
     if (is_queue_empty(queue_size)) {
-        cout << "Element cannot be removed, queue is empty\n";
+        if (flag) cout << "Element cannot be removed, queue is empty\n";
         return;
     }
-    cout << "Element ((" << queue_data[0].center.x << ", " << queue_data[0].center.y << "), " << queue_data[0].radius << ") ";
+    if (flag)
+        cout << "Element ((" << queue_data[0].center.x << ", " << queue_data[0].center.y << "), "
+             << queue_data[0].radius
+             << ") ";
     for (int i = 0; i < queue_size - 1; i++) {
         queue_data[i] = queue_data[i + 1];
     }
     queue_size--;
-    cout  << "removed;\n";
+    if (flag) cout << "removed;\n";
 }
 
-void dequeue(vector<Circle> &queue) {
+void dequeue(vector<Circle> &queue, int flag = 1) {
     if (is_queue_empty(queue)) {
-        cout << "Element cannot be removed, queue is empty\n";
+        if (flag) cout << "Element cannot be removed, queue is empty\n";
         return;
     }
-    cout << "Element ((" << queue.at(0).center.x << ", " << queue.at(0).center.y << "), " << queue.at(0).radius << ") ";
+    if (flag)
+        cout << "Element ((" << queue.at(0).center.x << ", " << queue.at(0).center.y << "), " << queue.at(0).radius
+             << ") ";
     queue.erase(queue.begin());
-    cout  << "removed;\n";
+    if (flag) cout << "removed;\n";
 }
 
 void input_values(double &x, double &y, double &radius) {
@@ -135,100 +145,124 @@ void input_values(double &x, double &y, double &radius) {
     cin >> radius;
 }
 
-void print_queue(List queue){
+void print_queue(List queue) {
     if (is_queue_empty(queue)) cout << "Queue is empty;\n";
     else {
-        Node* ptr = queue.head;
+        Node *ptr = queue.head;
         int i = 0;
-        while(ptr!=nullptr){
-            cout << ++i << ": ((" << ptr->circle.center.x << ", " << ptr->circle.center.y << "), " << ptr->circle.radius << ");\n";
+        while (ptr != nullptr) {
+            cout << ++i << ": ((" << ptr->circle.center.x << ", " << ptr->circle.center.y << "), " << ptr->circle.radius
+                 << ");\n";
             ptr = ptr->next;
         }
     }
 }
 
-void print_queue(Circle *queue_data, int &queue_size){
+void print_queue(Circle *queue_data, int &queue_size) {
     if (is_queue_empty(queue_size)) cout << "Queue is empty;\n";
     else {
         for (int i = 0; i < queue_size; i++) {
-            cout << i+1 << ": ((" << queue_data[i].center.x << ", " << queue_data[i].center.y << "), " << queue_data[i].radius << ");\n";
+            cout << i + 1 << ": ((" << queue_data[i].center.x << ", " << queue_data[i].center.y << "), "
+                 << queue_data[i].radius << ");\n";
         }
     }
 }
 
-void print_queue(vector<Circle> queue){
+void print_queue(vector<Circle> queue) {
     if (is_queue_empty(queue)) cout << "Queue is empty;\n";
     else {
         for (int i = 0; i < queue.size(); i++) {
-            cout << i+1 << ": ((" << queue[i].center.x << ", " << queue[i].center.y << "), " << queue[i].radius << ");\n";
+            cout << i + 1 << ": ((" << queue[i].center.x << ", " << queue[i].center.y << "), " << queue[i].radius
+                 << ");\n";
         }
     }
 }
 
-void demonstrate(List queue){
+void demonstrate(List queue) {
     create_empty(queue);
-    int num = (int)rand_num(5);
-    for(int i = 0; i< num; i++){
-        enqueue(queue, rand_num(10), rand_num(10),rand_num(10));
+    int num = (int) rand_num(5);
+    for (int i = 0; i < num; i++) {
+        enqueue(queue, rand_num(10), rand_num(10), rand_num(10));
     }
     cout << "Print:\n";
     print_queue(queue);
-    num = (int)rand_num(5);
-    for(int i = 0; i< num; i++){
+    num = (int) rand_num(5);
+    for (int i = 0; i < num; i++) {
         dequeue(queue);
     }
     cout << "Print:\n";
     print_queue(queue);
-    num = (int)rand_num(3);
-    for(int i = 0; i< num; i++){
-        enqueue(queue, rand_num(10), rand_num(10),rand_num(10));
+    num = (int) rand_num(3);
+    for (int i = 0; i < num; i++) {
+        enqueue(queue, rand_num(10), rand_num(10), rand_num(10));
     }
     cout << "Print:\n";
     print_queue(queue);
 }
 
-void demonstrate(Circle *queue_data, int &queue_size){
+void demonstrate(Circle *queue_data, int queue_size) {
+    Circle queue[MAX_SIZE];
+    for (int i = 0; i < queue_size; i++) queue[i] = queue_data[i];
+
     create_empty(queue_size);
-    int num = (int)rand_num(5);
-    for(int i = 0; i< num; i++){
-        enqueue(queue_data, queue_size, rand_num(10), rand_num(10),rand_num(10));
+    int num = (int) rand_num(5);
+    for (int i = 0; i < num; i++) {
+        enqueue(queue, queue_size, rand_num(10), rand_num(10), rand_num(10));
     }
     cout << "Print:\n";
-    print_queue(queue_data, queue_size);
-    num = (int)rand_num(5);
-    for(int i = 0; i< num; i++){
-        dequeue(queue_data, queue_size);
+    print_queue(queue, queue_size);
+    num = (int) rand_num(5);
+    for (int i = 0; i < num; i++) {
+        dequeue(queue, queue_size);
     }
     cout << "Print:\n";
-    print_queue(queue_data, queue_size);
-    num = (int)rand_num(3);
-    for(int i = 0; i< num; i++){
-        enqueue(queue_data, queue_size, rand_num(10), rand_num(10),rand_num(10));
+    print_queue(queue, queue_size);
+    num = (int) rand_num(3);
+    for (int i = 0; i < num; i++) {
+        enqueue(queue, queue_size, rand_num(10), rand_num(10), rand_num(10));
     }
     cout << "Print:\n";
-    print_queue(queue_data, queue_size);
+    print_queue(queue, queue_size);
 }
 
-void demonstrate(vector<Circle> queue){
+void demonstrate(vector<Circle> queue) {
     create_empty(queue);
-    int num = (int)rand_num(5);
-    for(int i = 0; i< num; i++){
-        enqueue(queue, rand_num(10), rand_num(10),rand_num(10));
+    int num = (int) rand_num(5);
+    for (int i = 0; i < num; i++) {
+        enqueue(queue, rand_num(10), rand_num(10), rand_num(10));
     }
     cout << "Print:\n";
     print_queue(queue);
-    num = (int)rand_num(5);
-    for(int i = 0; i< num; i++){
+    num = (int) rand_num(5);
+    for (int i = 0; i < num; i++) {
         dequeue(queue);
     }
     cout << "Print:\n";
     print_queue(queue);
-    num = (int)rand_num(3);
-    for(int i = 0; i< num; i++){
-        enqueue(queue, rand_num(10), rand_num(10),rand_num(10));
+    num = (int) rand_num(3);
+    for (int i = 0; i < num; i++) {
+        enqueue(queue, rand_num(10), rand_num(10), rand_num(10));
     }
     cout << "Print:\n";
     print_queue(queue);
+}
+
+template<typename Func>
+auto time(const Func &func, size_t n) {
+    const auto begin = system_clock::now();
+
+    for (size_t i = 0; i < n; i++)
+        func();
+
+    const auto end = system_clock::now();
+
+    return duration_cast<milliseconds>(end - begin).count();
+}
+
+void f(int val){
+    val++;
+    val++;
+    val*=2;
 }
 
 int main() {
@@ -255,13 +289,13 @@ int main() {
                 cin >> mode;
 
                 if ((mode < 1) || (mode > 3)) cout << "select correct mode\n";
-                else cout<< "Mode " << mode << " selected\n";
+                else cout << "Mode " << mode << " selected\n";
             } while ((mode < 1) || (mode > 3));
             continue;
         }
 
         if (str == "create") {
-            switch (mode){
+            switch (mode) {
                 case 0: {
                     cout << "Select mode first;\n";
                     break;
@@ -278,14 +312,15 @@ int main() {
                     create_empty(queue_vector);
                     break;
                 }
-                default: break;
+                default:
+                    break;
             }
             continue;
         }
 
         if (str == "add") {
             input_values(x, y, r);
-            switch (mode){
+            switch (mode) {
                 case 0: {
                     cout << "Select mode first;\n";
                     break;
@@ -302,13 +337,14 @@ int main() {
                     enqueue(queue_vector, x, y, r);
                     break;
                 }
-                default: break;
+                default:
+                    break;
             }
             continue;
         }
 
         if (str == "remove") {
-            switch (mode){
+            switch (mode) {
                 case 0: {
                     cout << "Select mode first;\n";
                     break;
@@ -325,13 +361,14 @@ int main() {
                     dequeue(queue_vector);
                     break;
                 }
-                default: break;
+                default:
+                    break;
             }
             continue;
         }
 
         if (str == "is_empty") {
-            switch (mode){
+            switch (mode) {
                 case 0: {
                     cout << "Select mode first;\n";
                     break;
@@ -348,13 +385,14 @@ int main() {
                     cout << boolalpha << is_queue_empty(queue_vector) << endl;
                     break;
                 }
-                default: break;
+                default:
+                    break;
             }
             continue;
         }
 
         if (str == "print") {
-            switch (mode){
+            switch (mode) {
                 case 0: {
                     cout << "Select mode first;\n";
                     break;
@@ -368,16 +406,17 @@ int main() {
                     break;
                 }
                 case 3: {
-                print_queue(queue_vector);
-                break;
+                    print_queue(queue_vector);
+                    break;
                 }
-                default: break;
+                default:
+                    break;
             }
             continue;
         }
 
         if (str == "demo") {
-            switch (mode){
+            switch (mode) {
                 case 0: {
                     cout << "Select mode first;\n";
                     break;
@@ -394,15 +433,46 @@ int main() {
                     demonstrate(queue_vector);
                     break;
                 }
-                default: break;
+                default:
+                    break;
             }
             continue;
         }
 
-        if (str == "help"){
+        if (str == "benchmark") {
+            cout << "Benchmark:\n";
+            const int N = 1000000;
+            x = rand_num(10);
+            y = rand_num(10);
+            r = rand_num(10);
+
+
+            cout << "center x = " << x << endl;
+            cout << "center y = " << y << endl;
+            cout << "radius = " << r << endl;
+
+
+            cout << "\ncreate_empty:\n";
+            cout << "list (" << N  << "): " << time([&queue_list](){create_empty(queue_list);}, N) << endl;
+            cout << "array (" << N  << "): " << time([&queue_array](){create_empty(queue_array.size);}, N) << endl;
+            cout << "vector (" << N  << "): " << time([&queue_vector](){create_empty(queue_vector);}, N) << endl;
+
+            cout << "\nenqueue:\n";
+            cout << "list (" << N  << "): " << time([&queue_list, &x, &y, &r](){enqueue(queue_list, x, y, r, 0);}, N) << endl;
+            cout << "array (" << N  << "): " << time([&queue_array, &x, &y, &r](){enqueue(queue_array.data, queue_array.size, x, y, r, 0);}, N) << endl;
+            cout << "vector (" << N  << "): " << time([&queue_vector, &x, &y, &r](){enqueue(queue_vector, x, y, r, 0);}, N) << endl;
+
+            cout << "\ndequeue:\n";
+            cout << "list  (" << N  << "): " << time([&queue_list](){dequeue(queue_list, 0);}, N) << endl;
+            cout << "array  (" << N  << "): " << time([&queue_array](){dequeue(queue_array.data, queue_array.size, 0);}, N) << endl;
+            cout << "vector  (" << (N/100000)  << "): " << time([&queue_vector](){dequeue(queue_vector, 0);}, (N/100000)) << endl;
+            continue;
+        }
+
+        if (str == "help") {
             cout << "  -stop\n"
                     "  -demo\n"
-                    "  -@benchmark\n"
+                    "  -benchmark\n"
                     "  -select_mode\n"
                     "  -create\n"
                     "  -add\n"
@@ -413,7 +483,7 @@ int main() {
         }
 
         cout << "Enter correct command (help)\n";
-
+//        cout << "\ncreate_empty:\n";
     }
     return 0;
 }
