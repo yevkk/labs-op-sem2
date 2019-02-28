4eb5bc91e2afc3d899579592361ce10e6f7b9461
yevkk <yevkk@users.noreply.github.com>
2019-02-28 19:01:37 +0200

    (lab 2b(16)) add message function added;

--- lab2b(16)/main.cpp
+++ lab2b(16)/main.cpp
@@ -4,7 +4,7 @@
 #include <ctime>
 using namespace std;
 
-const int MAX = 10;
+const int MAX_CAP = 10;
 
 struct Message {
     string text;
@@ -17,10 +17,30 @@ struct Node {
 };
 
 struct List {
-    Node* head;
-    Node* tail;
-    int capacity;
+    Node* head = nullptr;
+    Node* tail = nullptr;
+    int capacity = 0;
 };
+
+void add_msg(List &lst, string &msg){
+    if (lst.capacity == MAX_CAP){
+        lst.head->msg.text = msg;
+        lst.head->msg.time = chrono::system_clock::to_time_t(chrono::system_clock::now());
+        lst.tail = lst.head;
+        lst.head = lst.head->next;
+    } else {
+        Node *ptr = new Node;
+        if (lst.capacity == 0) lst.head = ptr;
+        ptr->msg.text = msg;
+        ptr->msg.time = chrono::system_clock::to_time_t(chrono::system_clock::now());
+        ptr->next = lst.head;
+        lst.tail->next = ptr;
+        lst.tail = ptr;
+        lst.capacity++;
+    }
+}
+
+
 
 int main() {
     List lst;
