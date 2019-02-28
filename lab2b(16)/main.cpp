77c6eaf3a5d6be0e2c0d10e84f0c97479c1036a9
yevkk <yevkk@users.noreply.github.com>
2019-02-28 21:33:57 +0200

    (lab 2b(16)) demo mode, console control added;

--- lab2b(16)/main.cpp
+++ lab2b(16)/main.cpp
@@ -44,16 +44,98 @@ void add_msg(List &lst, string &msg) {
 void print(List &lst, int k) {
     Node *ptr = lst.head;
     for (int i = 0; i < (lst.capacity - k); i++) ptr = ptr->next;
-    do{
+    do {
         cout << '"' << ptr->msg.text << "\"  " << ctime(&(ptr->msg.time));
         ptr = ptr->next;
-    }while (ptr != lst.head);
+    } while (ptr != lst.head);
+}
+
+void demonstration(List lst){
+    string msg;
+
+    cout << "Max capacity:" << MAX_CAP;
+
+    msg = "00000001";
+    add_msg(lst, msg);
+    cout << "Message \"" << msg << "\" added\n";
+
+    msg = "00000010";
+    add_msg(lst, msg);
+    cout << "Message \"" << msg << "\" added\n";
+
+    cout << "Print (2):\n";
+    print(lst, 2);
+
+    msg = "00000011";
+    add_msg(lst, msg);
+    cout << "Message \"" << msg << "\" added\n";
+
+    msg = "00000100";
+    add_msg(lst, msg);
+    cout << "Message \"" << msg << "\" added\n";
+
+    msg = "00000101";
+    add_msg(lst, msg);
+    cout << "Message \"" << msg << "\" added\n";
+
+    msg = "00000110";
+    add_msg(lst, msg);
+    cout << "Message \"" << msg << "\" added\n";
+    msg = "00000111";
+    add_msg(lst, msg);
+    cout << "Message \"" << msg << "\" added\n";
+
+    msg = "00001000";
+    add_msg(lst, msg);
+    cout << "Message \"" << msg << "\" added\n";
+
+    cout << "Print (3):\n";
+    print(lst, 3);
+
+    cout << "Print (1):\n";
+    print(lst, 1);
+
+    cout << "Print (2):\n";
+    print(lst, 2);
 }
 
 int main() {
-    List lst; string str, msg;
+    List lst;
+    string str, msg;
+    int k;
 
+    do {
+        cout << "\nEnter commmand: ";
+        cin >> str;
 
+        if (str == "add") {
+            cout << "Message text: ";
+            cin >> msg;
+            add_msg(lst, msg);
+            continue;
+        }
+
+        if (str == "print_k") {
+            do {
+                cout << "k (k <= " << MAX_CAP << ") = ";
+                cin >> k;
+            } while ((k <= 0) || (k > MAX_CAP));
+            print(lst, k);
+            continue;
+        }
+
+        if (str == "demo") {
+            demonstration(lst);
+            continue;
+        }
+
+        if (str == "help") {
+            cout << "-stop\n" << "-add\n" << "-print_k\n" << "-demo\n";
+            continue;
+        }
+
+        cout << "Enter correct command";
+    } while (str != "stop");
 
     return 0;
 }
