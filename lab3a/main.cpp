34f0209b3ea78cca7e31f82768642e849423d3a8
yevkk <yevkk@users.noreply.github.com>
2019-04-19 15:34:05 +0300

    (lab3a) quick sort added;

--- lab3a/main.cpp
+++ lab3a/main.cpp
@@ -8,8 +8,9 @@ const double MAX = 50;
 
 struct Point {
     double x, y, z;
+
     double dist() {
-        return sqrt((x*x) + (y*y) + (z*z));
+        return sqrt((x * x) + (y * y) + (z * z));
     }
 };
 
@@ -33,6 +34,16 @@ void swap(Point &pt1, Point &pt2) {
     pt1 = pt;
 }
 
+void print_array(Point *points, int n) {
+    for (int i = 0; i < n; i++) {
+        std::cout << "x: " << points[i].x << std::endl;
+        std::cout << "y: " << points[i].y << std::endl;
+        std::cout << "z: " << points[i].z << std::endl;
+        std::cout << "dist: " << points[i].dist() << std::endl << std::endl;
+    }
+}
+
+
 void bubble_sort(Point *points, int n) {
     bool flag;
     for (int i = 0; i < n - 1; i++) {
@@ -46,25 +57,39 @@ void bubble_sort(Point *points, int n) {
     }
 }
 
-void print_array(Point *points, int n) {
-    for (int i = 0; i < n; i++) {
-        std::cout << "x: " << points[i].x << std::endl;
-        std::cout << "y: " << points[i].y << std::endl;
-        std::cout << "z: " << points[i].z << std::endl;
-        std::cout << "dist: " << points[i].dist() << std::endl << std::endl;
+int partition(Point *points, int first, int last, char pivotMode = 'l') {
+    double pivot = (pivotMode == 'l') ? points[first].dist() : points[last].dist();
+    int i = first;
+    if (pivotMode == 'l') swap(points[first], points[last]);
+
+    for (int j = first; j <= last - 1; j++) {
+        if (points[j].dist() <= pivot) {
+            swap(points[i],
+                 points[j]);
+            i++;
+        }
+    }
+    swap(points[i], points[last]);
+    return i;
+}
+
+void quick_sort(Point *points, int first, int last, char pivotMode = 'l') {
+    if (first < last) {
+        int p = partition(points, first, last, pivotMode);
+        quick_sort(points, first, p - 1, pivotMode);
+        quick_sort(points, p + 1, last, pivotMode);
     }
 }
 
-
 int main() {
     Point points[N];
-    int size = 0, x=5;
+    int size = 0;
 
     for (int i = 0; i < 10; i++)
         add_element(points, size);
     print_array(points, size);
     std::cout << std::endl << std::endl << std::endl << std::endl << std::endl;
-    bubble_sort(&points[x], size-x);
+    quick_sort(points, 0, size - 1);
     print_array(points, size);
 
 
