47615fa6dcf7b64d7fc3d5e380516a918ab7e9ee
yevkk <yevkk@users.noreply.github.com>
2019-04-19 14:30:21 +0300

    (lab3a) types changed; bubble sort added;

--- lab3a/main.cpp
+++ lab3a/main.cpp
@@ -3,18 +3,14 @@
 #include <random>
 #include <ctime>
 
+const int N = 100;
+const double MAX = 50;
+
 struct Point {
     double x, y, z;
-};
-
-struct Node {
-    Node *next, *prev;
-    Point pt;
-};
-
-struct List {
-    Node *head;
-    int size;
+    double dist() {
+        return sqrt((x*x) + (y*y) + (z*z));
+    }
 };
 
 double rand_num(double min, double max) {
@@ -26,13 +22,51 @@ double rand_num(double min, double max) {
     return dist(gen);
 }
 
-double distance(Point pt) {
-    return sqrt((pt.x * pt.x) + (pt.y * pt.y) + (pt.z * pt.z));
+void add_element(Point *points, int &n) {
+    points[n] = {rand_num(-MAX, MAX), rand_num(-MAX, MAX), rand_num(-MAX, MAX)};
+    n++;
+}
+
+void swap(Point &pt1, Point &pt2) {
+    Point pt = pt2;
+    pt2 = pt1;
+    pt1 = pt;
+}
+
+void bubble_sort(Point *points, int n) {
+    bool flag;
+    for (int i = 0; i < n - 1; i++) {
+        flag = true;
+        for (int j = 0; j < n - i - 1; j++)
+            if (points[j].dist() > points[j + 1].dist()) {
+                flag = false;
+                swap(points[j], points[j + 1]);
+            }
+        if (flag) return;
+    }
+}
+
+void print_array(Point *points, int n) {
+    for (int i = 0; i < n; i++) {
+        std::cout << "x: " << points[i].x << std::endl;
+        std::cout << "y: " << points[i].y << std::endl;
+        std::cout << "z: " << points[i].z << std::endl;
+        std::cout << "dist: " << points[i].dist() << std::endl << std::endl;
+    }
 }
 
 
-
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Point points[N];
+    int size = 0, x=5;
+
+    for (int i = 0; i < 10; i++)
+        add_element(points, size);
+    print_array(points, size);
+    std::cout << std::endl << std::endl << std::endl << std::endl << std::endl;
+    bubble_sort(&points[x], size-x);
+    print_array(points, size);
+
+
     return 0;
 }
