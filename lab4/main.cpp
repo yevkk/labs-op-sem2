00460937ff1de62caad109dd02e54186cb35890b
yevkk <yevkk@users.noreply.github.com>
2019-05-01 19:48:56 +0300

    (lab4) unit 1 progress commit;

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,8 +1,27 @@
 // 1, 4, 8, 15, 18, 20, 23
 #include <iostream>
+#include <random>
+#include <ctime>
 
 //---------- TOOLS ----------
 
+double rand_double(double min, double max) {
+    static std::random_device rd;
+    static std::seed_seq seed{rd(), static_cast<unsigned int>(time(nullptr))};
+    static std::mt19937_64 gen(seed);
+    static std::uniform_real_distribution<double> dist(min, max);
+
+    return dist(gen);
+}
+
+int rand_int(int min, int max) {
+    static std::random_device rd;
+    static std::seed_seq seed{rd(), static_cast<unsigned int>(time(nullptr))};
+    static std::mt19937_64 gen(seed);
+    static std::uniform_int_distribution<int> dist(min, max);
+
+    return dist(gen);
+}
 
 //---------- Unit 0 (task 1) ----------
 const int MAX_CH = 10; //max number of children;
@@ -10,17 +29,37 @@ const int MAX_CH = 10; //max number of children;
 struct TreeNode {
     int data;
     int deg = 0;
-    TreeNode* children[MAX_CH];
+    TreeNode *children[MAX_CH] = {};
 
-    TreeNode(int Data){
+    explicit TreeNode(int Data) {
         data = Data;
-        for (int i = 0; i < MAX_CH; i++)
-            children[i] = nullptr;
+        for (auto &pt : children)
+            pt = nullptr;
     }
 };
 
 
+//---------- Block 1 (task 4) ----------
+const int MAX_VAL = 100;
+
+void add_node_to_tree(TreeNode *&root, int val, double p) {
+    double p1 = rand_double(0, 1);
+    if (root == nullptr) {
+        root = new TreeNode(val);
+    } else {
+        if ((root->deg < MAX_CH) && ((p1 <= p) || (root->deg == 0))) {
+            root->children[root->deg] = new TreeNode(val);
+            root->deg++;
+        } else {
+            add_node_to_tree(root->children[rand_int(0, root->deg - 1)], val, p);
+        }
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    TreeNode *root = nullptr;
+    for (int i = 0; i < 25; i++) {
+        add_node_to_tree(root, rand_int(0, MAX_VAL), rand_double(0, 1));
+    }
     return 0;
 }
