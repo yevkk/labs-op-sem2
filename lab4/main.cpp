c3e08bb685240fdb5211622a908b454db753cdee
yevkk <yevkk@users.noreply.github.com>
2019-05-03 11:56:16 +0300

    (lab4) added unit 3 (task 15);

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -39,7 +39,7 @@ struct TreeNode {
 };
 
 //---------- UNIT 1 (task 4) ----------
-const int MAX_VAL = 100;
+const int MAX_VAL = 5;
 
 void add_node_to_tree(TreeNode *&root, int val, double p) {
     double p1 = rand_double(0, 1);
@@ -56,15 +56,46 @@ void add_node_to_tree(TreeNode *&root, int val, double p) {
 }
 
 //---------- UNIT 2 (task 8) ----------
-void print_tree(TreeNode* root, int level = 0)
-{
-    if(root != nullptr){
-        for(int i = 0; i < level; i++)
-            std::cout << "\t";
+void print_tree(TreeNode *root, int level = 0) {
+    if (root != nullptr) {
+        for (int i = 0; i < 4 * level; i++)
+            std::cout << " ";
         std::cout << "|" << root->data << std::endl;
-        for(auto ch: root->children)
-            print_tree(ch, level+1);
+        for (auto ch: root->children)
+            print_tree(ch, level + 1);
     }
+}
+
+//---------- UNIT 3 (task 15) ----------
+void delete_children(TreeNode *&node) {
+    if (node != nullptr) {
+        for (auto& ch: node->children) {
+            delete_children(ch);
+            ch = nullptr;
+        }
+    }
+}
+
+void delete_nodes_from_tree(TreeNode *&root, int val) {
+    if (root != nullptr) {
+        if (root->data == val) {
+            delete_children(root);
+            delete root;
+            root = nullptr;
+        } else {
+            for (auto &ch:root->children) {
+                delete_nodes_from_tree(ch, val);
+            }
+            for (int i = 0; i < root->deg; i++){
+                if (root->children[i] == nullptr) {
+                    root->children[i] = root->children[root->deg-1];
+                    root->children[root->deg-1] = nullptr;
+                    root->deg--;
+                }
+            }
+        }
+    }
+
 }
 
 int main() {
@@ -72,6 +103,11 @@ int main() {
     for (int i = 0; i < 25; i++) {
         add_node_to_tree(root, rand_int(-MAX_VAL, MAX_VAL), rand_double(0, 1));
     }
+    print_tree(root);
+    int c;
+    std::cin >> c;
+    TreeNode* k = root;
+    delete_nodes_from_tree(root, c);;
     print_tree(root);
     return 0;
 }
